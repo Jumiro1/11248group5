0b1d9bfb6c29867aa9da721ce12d170d88a1418a
Mstriker64 <mitchellthanath@gmail.com>
2021-10-25 16:07:19 -0400

    Update Login.js

--- Energy-Optimizer/frontend/src/components/Login.js
+++ Energy-Optimizer/frontend/src/components/Login.js
@@ -4,16 +4,7 @@ import {Button, Col, Container, Form, Row, Card} from "react-bootstrap";
 function Login(){
     return (
         <div className="App">
-            Login 
-            
-            <br>
-            </br>
-            <br>
-            </br>
-            <br>
-            </br>
-            <br>
-            </br>
+           
             
             <Container>
                 <Row>
@@ -22,21 +13,26 @@ function Login(){
                     </Col>
 
                     <Col lg={4}md={6} sm ={12}>
+                    <br></br>
+                    <br></br>
+                    <h1>Login to your account</h1>
+                    <br></br>
+                    <hr />
                         <Form>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <Form.Label>Email address</Form.Label>
+                            <Form.Label><h4>Email address</h4></Form.Label>
                             <Form.Control type="email" placeholder="Enter email" />
                             <Form.Text className="text-muted">
-                            We'll never share your email with anyone else.
+                            (We'll never share your email with anyone else.)
                             </Form.Text>
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
+                            <Form.Label><h4>Password</h4></Form.Label>
                             <Form.Control type="password" placeholder="Password" />
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                            <Form.Check type="checkbox" label="Save Password                           " />
+                            <Form.Check type="checkbox" label="Save Password     " />
                         </Form.Group>
 
                         <br>
